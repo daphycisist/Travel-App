8fbeb787debbfe1ac78fce46d1a20581ebd63157
daphycisist <daphycisist@gmail.com>
2020-12-16 16:26:09 +0100

    add Navbar and Top Section

---
+++ travel-app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact strict component={ Home } />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
